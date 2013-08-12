29437ec07ae457351172ce9e2ffa77b96043077c
nakajmg <nakajima.jmg@gmail.com>
2013-08-13 01:02:06 +0900

    おみくじ作成途中

---
+++ chapter3/lib/js/init.js
@@ -0,0 +1,17 @@
+function id(elementId) {
+    return document.getElementById(elementId);
+}
+
+function tag(tagName, tagNo) {
+    tagNo = tagNo | 0;
+    var elements = document.getElementsByTagName(tagName);
+    if ( tagNo <= elements.length ) {
+        return elements[tagNo];
+    } else {
+        return null;
+    }
+}
+
+function random(min, max) {
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
